dcd7997325a679e38b4eb5cc1b59aba5ccbb4953
Jim Jin <Zhenglin.jin1216@hotmail.com>
2023-01-06 20:43:14 +0800

    Initial commit

---
+++ app/src/main/java/com/jimjin/thehungrydeveloper/MainActivity.java
@@ -0,0 +1,59 @@
+package com.jimjin.thehungrydeveloper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
+    CardView dessertsCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
+        dessertsCard = findViewById(R.id.card_view_desserts);
+
+        startersCard.setOnClickListener(view -> {
+
+            Intent startersActivityIntent = new Intent(MainActivity.this, StarterActivity.class);
+
+            startActivity(startersActivityIntent);
+        });
+
+        mainsCard.setOnClickListener(view -> {
+            Intent mainCourseActivityIntent = new Intent(MainActivity.this,MainCoursesActivity.class);
+            startActivity(mainCourseActivityIntent);
+        });
+
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dessertsActivityIntent = new Intent(MainActivity.this,DessertsActivity.class);
+                startActivity(dessertsActivityIntent);
+            }
+        });
+
+        TextView emailTextView = findViewById(R.id.text_view_email_address);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent launchEmailAppIntent = new Intent(Intent.ACTION_SENDTO);
+                launchEmailAppIntent.setData(Uri.parse("mailto:thehungrydeveloper@jimjin.com")); // only email apps should handle this
+                startActivity(launchEmailAppIntent);
+            }
+        });
+
+
+
+    }
+}
